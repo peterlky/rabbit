6418af9d203a3edde92a95662a39340d7b9b9f2c
Peter <Peter@Nelsonde-MacBook-Pro.local>
2016-06-02 18:05:07 +0800

    5

--- emit_log.py
+++ emit_log.py
@@ -9,7 +9,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='logs',
                          type='fanout')
 
-message = ' '.join(sys.argv[1:]) or "info: Hello World!XdasdsadasdsaXXXXX"
+message = ' '.join(sys.argv[1:]) or "info: Hello World!XdasdsadasdsafdsfsdfdsaXXXXX"
 channel.basic_publish(exchange='logs',
                       routing_key='',
                       body=message)
